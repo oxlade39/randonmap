b491a3f5d3f26e1703e7e19f94130b8ee486e303
Dan Oxlade <dan.oxlade@gmail.com>
2020-04-20 09:17:13 +0100

    Updates

---
+++ precache-manifest.c6c45e0c9f050654eb8b7692a55f2e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa859161e8758f4c84e62de7211b61b4",
+    "url": "/randommap/index.html"
+  },
+  {
+    "revision": "bf4bdbfd58f78482f0b8",
+    "url": "/randommap/static/css/main.c1798b61.chunk.css"
+  },
+  {
+    "revision": "14caae322a6ef6b4e5d5",
+    "url": "/randommap/static/js/2.cd846438.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/randommap/static/js/2.cd846438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4bdbfd58f78482f0b8",
+    "url": "/randommap/static/js/main.ef4f4a17.chunk.js"
+  },
+  {
+    "revision": "2e9a124900812f61a9d9",
+    "url": "/randommap/static/js/runtime-main.27e88387.js"
+  }
+]);
